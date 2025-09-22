310721e1390fff15f1068277bec9a8b8420d925b
kitoito8787 <mainichi8787@gmail.com>
2025-09-22 22:04:33 +0900

    Add files via upload

---
+++ associative-array.php
@@ -0,0 +1,11 @@
+<?php
+$vegetable = [
+  "name" => "onion",
+  "price" => 200,
+  "weight" => 160
+];
+
+// 動作確認
+echo $vegetable["name"];   // onion
+echo $vegetable["price"];  // 200
+echo $vegetable["weight"]; // 160
